9e10ea5b12c39ef155676e84e4109873b271e78a
shiruiz2208 <gise.13.ruiz@gmail.com>
2023-11-14 04:19:30 -0300

    agregamos ejercicio 6

--- conceptosBasicosJS-c74i-arrays/js/5-arrays.js
+++ conceptosBasicosJS-c74i-arrays/js/5-arrays.js
@@ -7,6 +7,7 @@ const peliculas = ['dr strange','rambo','La saga de Transformer','Five nights at
 console.log(peliculas.length);
 console.log(peliculas.length - 1);
 
+mostrarPeliculas('');
 //mostrar por pantalla un array
 document.write(peliculas);
 document.write(`<p>El primer elemento del array es: ${peliculas[0]}</p>`);
@@ -22,62 +23,25 @@ document.write(`</ul>`);
 
 //agregar elementos en el array
 peliculas.unshift('Ironman 1','Ironman 2');
-document.write(`<h2>Lista de peliculas + ironman</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarPeliculas('+Iroman')
+
 
 peliculas.splice(2,0,'Thor');
-document.write(`<h2>Lista de peliculas + thor</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarPeliculas('+Thor')
 
 peliculas.push('Batman');
-document.write(`<h2>Lista de peliculas + Batman</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarPeliculas ('+Batman')
 
 //modificar elementos en el array
 peliculas[3] = 'Dr. Strange: multiverse of madness'
 console.log(peliculas)
-document.write(`<h2>Lista de peliculas + Dr. strange modificado (${peliculas.length})</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarPeliculas(` + Dr. strange modificado (${peliculas.length})`);
+
 
 //borrar elementos del array
 peliculas.shift();
-document.write(`<h2>Lista de peliculas menos el primer elemento (${peliculas.length})</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarPeliculas(`menos el primer elemento(${peliculas.length})`)
 
-// peliculas.splice(6,1);
-peliculas.splice(6,2);
-// peliculas.splice(6);
-document.write(`<h2>Lista de peliculas menos un elemento particular (${peliculas.length})</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
 
 peliculas.pop();
-document.write(`<h2>Lista de peliculas menos el ultimo elemento (${peliculas.length})</h2>`);
-document.write(`<ul>`);
-for(let i=0; i < peliculas.length; i++){
-    document.write(`<li>${peliculas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarPeliculas (`menos el ultimo elemento(${peliculas.length})`)
--- conceptosBasicosJS-c74i-arrays/js/6-funciones.js
+++ conceptosBasicosJS-c74i-arrays/js/6-funciones.js
@@ -5,8 +5,27 @@ function saludar (){
 function nuevoSaludo(nombreIngresado,apellidoIngresado){
     document.write (`<p>Hola ${nombreIngresado} , ${apellidoIngresado} x,que tengass buen día</p>`)
 }
-//invocar a una funcion
 
+ //function conversionApesos(dolares){
+ //   const precioFinal= dolares * 1000;
+   // return precioFinal;
+    //console.log(precioFinal);
+ //}
+ //
+
+//arrow functions
+//const conversionApesos = dolores =>{
+   // const precioFinal = dolores *1000;
+    //console.log (precioFinal);
+    //return precioFinal;
+//}
+ const conversionApesos= (dolares) => dolares *1000;
+
+
+
+
+
+//invocar a una funcion
 saludar();
 
 const nombre = prompt('Ingrese un nombre');
@@ -18,3 +37,11 @@ nuevoSaludo ( 'Luciano','Cabello');
 
 nuevoSaludo('Sebastian');
 nuevoSaludo();
+
+
+conversionApesos(35);
+
+let precioproducto = conversionPesos(35);
+document.write(`<p>El precio del producto de prueba es ${precioProducto} </p>`);
+document.write(`<p>El precio del producto de prueba es ${conversionApesos} </p>`);
+
